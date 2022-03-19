87ba7d68608e99df85f73843f45a7a0cc97ea9a3
V-e-d <96762571+V-e-d@users.noreply.github.com>
2022-03-19 18:10:49 +0530

    Create oddsum.py

---
+++ Ved/oddsum.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first number \n"))
+b=int(input("Enter second number \n"))
+c=int(input("Enter third number \n"))
+d=int(input("Enter fourth number \n"))
+e=int(input("Enter fifth number \n"))
+add=a+b+c+d+e
+print("The sum of first 5 odd numbers is ",add)
