05072c2b7d63206773a6d003c61716c5bd62e453
anand8936 <101488060+anand8936@users.noreply.github.com>
2022-03-19 21:48:42 +0530

    Create rectangle.py

---
+++ Anand/rectangle.py
@@ -0,0 +1,7 @@
+#code
+length = int(input("enter the length of rectangle"))
+breadth =int(input("enter the breadth of rectangle"))
+area = length * breadth
+perimeter = 2 * (length + breadth)
+print("area",area)
+print("perimeter",perimeter)
