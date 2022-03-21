a97f63034a94c43a538d2c13d13740484f3fcaac
V-e-d <96762571+V-e-d@users.noreply.github.com>
2022-03-21 14:18:05 +0530

    Create rectangle.py

---
+++ Ved/rectangle.py
@@ -0,0 +1,6 @@
+length=int(input("Enter the length of rectangle "))
+breadth=int(input("Enter the breadth of rectangle "))
+area=length*breadth
+perimeter=2*(length+breadth)
+print("The area of rectangle is ",area)
+print("The perimeter of rectangle is ",perimeter)
