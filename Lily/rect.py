30569e8cd1326f05e83e04154f869a6442945692
Lily Kumari <101217376+Lily333-bot@users.noreply.github.com>
2022-03-21 15:55:32 +0530

    Create rect.py

---
+++ Lily/rect.py
@@ -0,0 +1,7 @@
+#Write a program in Python to find the Area and Perimeter of a Rectangle
+l=int(input("Enter Length:"))
+b=int(input("Enter Breadth:"))
+area=l*b
+perimeter=2*(l+b)
+print("Area=",area)
+print("Perimeter=",perimeter)
