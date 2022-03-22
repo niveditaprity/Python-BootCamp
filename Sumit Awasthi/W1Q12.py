204e66fd0a41cbf776510688a848ec11011d5c0e
Sumit Awasthi <93317934+asumit499@users.noreply.github.com>
2022-03-22 23:23:46 +0530

    Create W1Q12.py

---
+++ Sumit Awasthi/W1Q12.py
@@ -0,0 +1,6 @@
+r = int(input("Enter radius of circle: "))
+pi = 3.14
+area = pi*r*r
+circum = 2*pi*r
+print("Area of circle is ",area ,'sq units.')
+print("Circumference of circle is ",circum ,'units.')
