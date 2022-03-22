d35283db63ea7ed5c934439c9735d01f7d2c7dc9
singhs1234 <77673888+singhs1234@users.noreply.github.com>
2022-03-22 19:55:46 +0530

    Create volume_of_cylinder.py

---
+++ Swati Singh/volume_of_cylinder.py
@@ -0,0 +1,5 @@
+# 16. Write a program in Python to calculate the volume of a cylinder
+r=int(input("Enter radius of the cylinder: "))
+h=int(input("Enter height of the cylinder: "))
+vol=3.14*(r**2)*h
+print("Volume of the cylinder= ",vol)
