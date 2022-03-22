151a3cee27ebeb1d0cbe5d7a3c69eca5ad50d670
Sumit Awasthi <93317934+asumit499@users.noreply.github.com>
2022-03-22 23:36:10 +0530

    Create W1Q17.py

---
+++ Sumit Awasthi/W1Q17.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter 1st number: "))
+n2 = int(input("Enter 2nd number: "))
+n3 = int(input("Enter 3rd number: "))
+n4 = int(input("Enter 4th number: "))
+sum = n1+n2+n3+n4
+print("Total of numbers is", sum)
+print("Average of numbers is", sum/4)
