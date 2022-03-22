8cc28a32da74b840598ff9e2bb57f236ed2227fd
singhs1234 <77673888+singhs1234@users.noreply.github.com>
2022-03-22 19:50:22 +0530

    Create sum_of_two_user_inputs.py

---
+++ Swati Singh/sum_of_two_user_inputs.py
@@ -0,0 +1,5 @@
+# 8. Write a program in Python to add two numbers accepted through keyboard.
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+sum=a+b
+print("Sum of a and b= ",sum)
