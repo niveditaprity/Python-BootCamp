68c2365248c013f44f5f05e586ef661669f89980
anand8936 <101488060+anand8936@users.noreply.github.com>
2022-03-25 23:19:04 +0530

    Create third_angle.py

---
+++ third_angle.py
@@ -0,0 +1,5 @@
+angle1=int(input("Enter the first angle of the triangle"))
+angle2=int(input("Enter the second angle of the triangle"))
+sum=angle1+angle2
+angle3=180-sum
+print("Third angle of the triangle is=",angle3)
