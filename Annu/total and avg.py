9fb200103126a95f17568bbd2fbe77f521d8a046
AnnuK2 <101409955+AnnuK2@users.noreply.github.com>
2022-03-25 20:42:57 +0530

    Create total and avg.py

---
+++ Annu/total and avg.py
@@ -0,0 +1,8 @@
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+c=int(input("enter number:"))
+d=int(input("enter number:"))
+total=a+b+c+d
+average=total/4
+print("total=",total)
+print("average=",average)
