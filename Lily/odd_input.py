9c22b80e2f8d81a5da7b66c141221942bb49308b
Lily Kumari <101217376+Lily333-bot@users.noreply.github.com>
2022-03-27 14:16:39 +0530

    Create odd_input.py

---
+++ Lily/odd_input.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter 1st odd no: \n"))
+n2=int(input("Enter 2nd odd no: \n"))
+n3=int(input("Enter 3rd odd no: \n"))
+n4=int(input("Enter 4th odd no: \n"))
+n5=int(input("Enter 5th odd no: \n"))
+sum=n1+n2+n3+n4+n5
+print("Sum=",sum)
