66f2338a88cca38b29aafc72430f954b3cdbd3c1
V-e-d <96762571+V-e-d@users.noreply.github.com>
2022-03-30 23:23:43 +0530

    Create W2_Q3.py

---
+++ Ved/W2_Q3.py
@@ -0,0 +1,6 @@
+num=int(input("Enter any number: "))
+N=num%10
+if N/3==3 or 2 or 1:
+  print("The last digit of the number is divisible by 3")
+else:
+  print("The last digit of the number is not divisible by 3")
