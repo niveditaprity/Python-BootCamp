59537abfbadeb4a43af7221aedb7b5203296288e
V-e-d <96762571+V-e-d@users.noreply.github.com>
2022-03-31 19:21:11 +0530

    Create W2_Q11.py

---
+++ Ved/W2_Q11.py
@@ -0,0 +1,17 @@
+num=int(input("Enter any number from 1 to 7:"))
+if num==1:
+    print("Sunday")
+elif num==2:
+    print("Monday")
+elif num==3:
+    print("Tuesday")
+elif num==4:
+    print("Wednesday")
+elif num==5:
+    print("Thursday")
+elif num==6:
+    print("Friday")
+elif num==7:
+    print("Saturday")
+else:
+    print("You entered wrong number")
