ea2fb82b9576103cf78b5dc765ef3dd0fcb83a35
V-e-d <96762571+V-e-d@users.noreply.github.com>
2022-04-01 21:20:34 +0530

    Create W2_Q14.py

---
+++ Ved/W2_Q14.py
@@ -0,0 +1,7 @@
+num=int(input("Enter your age:"))
+if num>=60:
+    print("You are senior citizen.")
+elif num<60 and num>=0:
+    print("You are not senior citizen.")
+else:
+    print("You entered invalid age.")
