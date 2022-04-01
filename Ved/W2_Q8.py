f29812c480d23dc223ef9350f4ec91a57e6407b6
V-e-d <96762571+V-e-d@users.noreply.github.com>
2022-04-02 00:04:08 +0530

    Create W2_Q8.py

---
+++ Ved/W2_Q8.py
@@ -0,0 +1,11 @@
+marks=int(input("Enter your marks out of 100:"))
+if marks>90:
+    print("Grade A")
+elif marks>80:
+    print("Grade B")
+elif marks>60:
+    print("Grade C")
+elif marks>=0:
+    print("Grade D")
+else :
+    print("You entered incorrect marks")
