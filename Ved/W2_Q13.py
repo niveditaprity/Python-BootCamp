1cd42c29871f33c522bf9c6cf94b60426e31ced4
V-e-d <96762571+V-e-d@users.noreply.github.com>
2022-04-02 10:03:30 +0530

    Create W2_Q13.py

---
+++ Ved/W2_Q13.py
@@ -0,0 +1,10 @@
+print("Delhi\nAgra\nJaipur")
+city=input("Enter the name of the any city given above:")
+if city=="Delhi":
+    print("Red Fort")
+elif city=="Agra":
+    print("Taj Mahal")
+elif city=="Jaipur":
+    print("Jal Mahal")
+else:
+    print("You enterd wrong city.")
