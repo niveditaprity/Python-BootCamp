39e60dad7b8603070ddfefe2267faf752cf5c72b
Sumit Awasthi <93317934+timus69@users.noreply.github.com>
2022-04-25 14:05:26 +0530

    Create W2Q15.py

---
+++ Sumit Awasthi/W2Q15.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1>num2:
+    print("Second number is smaller.")
+elif num2>num1:
+    print("First number is smaller.")
+else:
+    print("Both numbers are equal.")
