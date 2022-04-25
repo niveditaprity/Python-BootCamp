f96108867a76e0db812db741a1086ebaf10cc4ce
singhs1234 <77673888+singhs1234@users.noreply.github.com>
2022-04-25 12:51:59 +0530

    Create multiple_of_5.py

---
+++ Swati Singh/multiple_of_5.py
@@ -0,0 +1,6 @@
+# 6. Write a program to display "Hello" if a number entered by user is a multiple of five , otherwise print "Bye"
+num=int(input("Enter any number: "))
+if num%5==0:
+    print("Hello")
+else:
+    print("Bye") 
