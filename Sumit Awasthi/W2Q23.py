2520a1615df1e0d264ec5bcc5123f2fa9ed1a8a1
Sumit Awasthi <93317934+timus69@users.noreply.github.com>
2022-04-25 17:48:46 +0530

    Create W2Q23.py

---
+++ Sumit Awasthi/W2Q23.py
@@ -0,0 +1,9 @@
+p = int(input("Enter the percentage: "))
+if p <40:
+    print("Fail")
+elif p>=40 and p<55 :
+    print("Fair")
+elif p>=55 and p<65:
+    print("Good")
+else:
+    print("Excellent")
